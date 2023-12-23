4e669d555f2e8be007d7c257c78e76b1ed7fbdaa
Nikitin Roman <78508681+IronWillDevops@users.noreply.github.com>
2023-12-23 19:12:50 +0200

    Create ui.js

---
+++ scripts/ui.js
@@ -0,0 +1,10 @@
+
+var settingsContainer = document.getElementById('settingsContainer');
+
+function openSettings() {
+    settingsContainer.style.display = 'flex';
+}
+
+function closeSettings() {
+   settingsContainer.style.display = 'none';
+}
